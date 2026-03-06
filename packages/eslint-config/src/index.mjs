75eddd37c1dd8cdd363534978ca2442f3a1ced55
allohamora <herman30698@gmail.com>
2026-03-06 13:18:38 +0200

    chore(eslint-config): add eslint-config package

---
+++ packages/eslint-config/src/index.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import beautifulSort from 'eslint-plugin-beautiful-sort';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  { ignores: ['node_modules', 'dist'] },
+  {
+    files: ['**/*.ts'],
+    languageOptions: { globals: { ...globals.node }, parserOptions: { project: true } },
+    plugins: {
+      'beautiful-sort': beautifulSort,
+    },
+    rules: {
+      'object-shorthand': 'warn',
+      'no-async-promise-executor': 'warn',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-misused-promises': 'warn',
+      '@typescript-eslint/no-deprecated': 'error',
+      'beautiful-sort/import': [
+        'error',
+        {
+          special: ['./mocks/index.js', '/config.service.mock.js$/', '/setup-.+-context.js$/'],
+          order: ['special', 'namespace', 'default', 'defaultObj', 'obj', 'none'],
+        },
+      ],
+      'no-relative-import-paths/no-relative-import-paths': ['error', { allowSameFolder: true, allowedDepth: 1 }],
+    },
+  },
+);
