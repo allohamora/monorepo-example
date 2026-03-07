2af77ea97691889646252ac64e327194882b6ea7
allohamora <herman30698@gmail.com>
2026-03-07 11:49:30 +0200

    test(client): add __tests__ dir

--- apps/client/__tests__/app.spec.ts
+++ apps/client/__tests__/app.spec.ts
@@ -1,5 +1,5 @@
 import { afterEach, describe, expect, it, vi } from 'vitest';
-import { getDateLabel } from './app.tsx';
+import { getDateLabel } from '../src/app.tsx';
 
 describe('app', () => {
   describe('getDateLabel', () => {
